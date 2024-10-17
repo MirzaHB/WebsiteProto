5ac4848085b93d11d35348fda7d4d3921d0a6279
MirzaHB <hassan.baig@ucalgary.ca>
2024-10-17 01:32:34 -0600

    fixing deployment routing

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,16 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/WebsiteProto/",
   plugins: [
     mdx({
       remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
       rehypePlugins: [],
       providerImportSource: "@mdx-js/react",
     }),
-    remix(),
+    remix({
+      basename: "/WebsiteProto/",
+    }),
     tsconfigPaths(),
   ],
   resolve: {
