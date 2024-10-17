d9693a3010daf73801a409d9c6a272771e7863d9
MirzaHB <hassan.baig@ucalgary.ca>
2024-10-17 00:40:48 -0600

    work on blogs

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "@remix-run/react";
 
 export default function BlogLayout() {
   return (
-    <div className="max-w-4xl mx-auto p-6">
-      <article className="prose lg:prose-xl">
+    <div className="max-w-5xl mx-auto p-6">
+      <article className="prose lg:prose-2xl">
         <Outlet />
       </article>
     </div>
