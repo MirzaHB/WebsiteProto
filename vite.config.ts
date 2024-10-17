57fe31939f2520626ed9cc95123998728e0bdac4
MirzaHB <hassan.baig@ucalgary.ca>
2024-10-17 11:28:41 -0600

    fixed an issue with pics not running in prod

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,13 @@ import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import { vitePlugin as remix } from "@remix-run/dev";
 import tsconfigPaths from "vite-tsconfig-paths";
+import rehypeHighlight from "rehype-highlight";
 
 export default defineConfig({
   plugins: [
     mdx({
       remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
-      rehypePlugins: [],
+      rehypePlugins: [rehypeHighlight],
       providerImportSource: "@mdx-js/react",
     }),
     remix({
